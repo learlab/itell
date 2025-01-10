e931329f9ad2eb95f0e00a3393e0b7fd08fda53b
Qiushi Yan <qiushi.yann@gmail.com>
2025-01-10 23:18:24 +0800

    Update page-status-modal.tsx

--- apps/demo/src/app/(textbook)/[slug]/_components/page-status-modal.tsx
+++ apps/demo/src/app/(textbook)/[slug]/_components/page-status-modal.tsx
@@ -17,7 +17,6 @@ import { type User } from "lucia";
 import { TakeConsent } from "@/components/take-consent";
 import { isProduction } from "@/lib/constants";
 import { type PageStatus } from "@/lib/page-status";
-import { firstAssignmentPage } from "@/lib/pages/pages.server";
 import { makePageHref } from "@/lib/utils";
 
 type Props = {
