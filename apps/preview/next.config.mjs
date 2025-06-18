21454fa29222ec6b1e86b58049e2df5b1f433faf
Qiushi Yan <qiushi.yann@gmail.com>
2025-06-17 23:00:04 -0500

    add image host

--- apps/preview/next.config.mjs
+++ apps/preview/next.config.mjs
@@ -1,34 +1,38 @@
 const isDev = process.argv.indexOf("dev") !== -1;
 const isBuild = process.argv.indexOf("build") !== -1;
 if (!process.env.VELITE_STARTED && (isDev || isBuild)) {
-	process.env.VELITE_STARTED = "1";
-	const { build } = await import("velite");
-	await build({ watch: isDev, clean: !isDev });
+  process.env.VELITE_STARTED = "1";
+  const { build } = await import("velite");
+  await build({ watch: isDev, clean: !isDev });
 }
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	reactStrictMode: false,
+  reactStrictMode: false,
 
-	images: {
-		dangerouslyAllowSVG: true,
-		remotePatterns: [
-			{
-				protocol: "https",
-				hostname: "nbjrajrmujlgxmcvqsge.supabase.co",
-			},
-			{
-				protocol: "https",
-				hostname: "cdn.simpleicons.org",
-			},
-		],
-	},
-	eslint: {
-		ignoreDuringBuilds: true,
-	},
-	experimental: {
-		optimizePackageImports: ["shiki"],
-	},
+  images: {
+    dangerouslyAllowSVG: true,
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "nbjrajrmujlgxmcvqsge.supabase.co",
+      },
+      {
+        protocol: "https",
+        hostname: "pxeblicvfnzlnounkznu.supabase.co",
+      },
+      {
+        protocol: "https",
+        hostname: "cdn.simpleicons.org",
+      },
+    ],
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  experimental: {
+    optimizePackageImports: ["shiki"],
+  },
 };
 
 export default nextConfig;
