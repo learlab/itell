c150a77231c9adce297cc37902986982741eca5b
Qiushi Yan <qiushi.yann@gmail.com>
2025-07-29 00:06:46 -0500

    fix image

--- apps/preview/src/lib/shared-components.tsx
+++ apps/preview/src/lib/shared-components.tsx
@@ -1,14 +1,13 @@
 import { Accordion, AccordionItem } from "@/components/ui/accordion";
 import { BlockquoteWrapper } from "@/components/ui/blockquote";
 import { Callout } from "@/components/ui/callout";
-import { Sandbox } from "@/components/ui/sandbox";
 import { StepsWrapper } from "@/components/ui/steps";
 import Image from "next/image";
 import { Question } from "./question";
 import { CodeBlockPython } from "./code-block";
 export const components = {
-  "i-sandbox-js": Sandbox,
-  "i-image": Image,
+  // "i-sandbox-js": Sandbox,
+  "i-image": (props: any) => <Image {...props} children={undefined} />,
   "i-blockquote": BlockquoteWrapper,
   "i-callout": Callout,
   "i-accordion": Accordion,
